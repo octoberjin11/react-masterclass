2b65b0bd9a80f0ee47e4e77209698502eedc12b1
joanna <joanna1011@greenit.cc>
2022-12-28 01:51:25 +0900

    #3.3 Optional Props

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,20 +1,16 @@
 import styled from "styled-components";
 
-interface ContainerProps {
+interface CircleProps {
     bgColor: string;
 }
 
 // styled.div<ContainerProps> : Container 가 받는 props를 TypeScript에게 설명해준다.
-const Container = styled.div<ContainerProps>`
+const Container = styled.div<CircleProps>`
     width: 200px;
     height: 200px;
     background-color: ${(props) => props.bgColor};
     border-radius: 100px;
 `;
-
-interface CircleProps {
-    bgColor: string;
-}
 
 function Circle({ bgColor }: CircleProps) {
     return <Container bgColor={bgColor} />;
