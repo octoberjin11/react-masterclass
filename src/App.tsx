4cfb489b262212d24299562e044a43e5c00ccf39
joanna <joanna1011@greenit.cc>
2023-01-05 00:28:10 +0900

    #3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.div`
+    color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-    const [value, setValue] = useState("");
-    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-        // event: React.FormEvent<HTMLInputElement>
-        // 이 onChange 함수가 InputElement에 의해서 실행될 것을 알림.
-
-        const {
-            currentTarget: { value },
-        } = event;
-        setValue(value);
-    };
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log("hello", value);
-    };
-
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={value}
-                    onChange={onChange}
-                    type="text"
-                    placeholder="username"
-                />
-                <button>Log in</button>
-            </form>
-        </div>
+        <Container>
+            <H1>protected</H1>
+        </Container>
     );
 }
 
