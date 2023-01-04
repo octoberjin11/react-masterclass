ae7d239b653edd038a433a3b7501290ab2aefea1
joanna <joanna1011@greenit.cc>
2023-01-05 00:11:34 +0900

    #3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+    const [value, setValue] = useState("");
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        // event: React.FormEvent<HTMLInputElement>
+        // 이 onChange 함수가 InputElement에 의해서 실행될 것을 알림.
+
+        const {
+            currentTarget: { value },
+        } = event;
+        setValue(value);
+    };
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("hello", value);
+    };
+
     return (
         <div>
-            <Circle borderColor="red" bgColor="teal" />
-            <Circle text="I'm here" bgColor="tomato" />
+            <form onSubmit={onSubmit}>
+                <input
+                    value={value}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="username"
+                />
+                <button>Log in</button>
+            </form>
         </div>
     );
 }
